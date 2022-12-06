e49522a11f35f330fd0adf5a020783ff039b28ec
Ermand Durro <ermand.duro@gmail.com>
2022-12-06 20:17:38 +0100

    Replace nvim-tree with neo-tree and add catppuccin color scheme

--- .config/nvim/lua/ermand/core/colorscheme.lua
+++ .config/nvim/lua/ermand/core/colorscheme.lua
@@ -1,6 +1,32 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+
+-- vim.g.tokyonight_style = "night"
+-- vim.g.tokyonight_italic_functions = true
+-- vim.g.tokyonight_transparent = true
+-- vim.g.tokyonight_transparent_sidebar = true
+--
+-- vim.g.tokyonight_colors = {
+-- 	fg = "#839496",
+-- 	fg_dark = "#586e75",
+-- 	fg_gutter = "#073642",
+-- 	bg_highlight = "#002b36",
+-- 	comment = "#586e75",
+-- 	blue = "#268bd2",
+--
+-- 	cyan = "#2aa198",
+-- 	blue1 = "#2ac3de",
+-- 	blue2 = "#0db9d7",
+-- 	blue5 = "#89ddff",
+-- 	blue6 = "#B4F9F8",
+--
+-- 	yellow = "#b58900",
+-- 	orange = "#cb4b16",
+-- 	magenta = "#d33682",
+-- 	purple = "#6c71c4",
+-- }
+--
+local status, _ = pcall(vim.cmd, "colorscheme catppuccin")
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
 	return
