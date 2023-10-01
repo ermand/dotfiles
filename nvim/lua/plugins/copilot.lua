a58e06008e8783b9b05445f6b7ae53727e06bf78
Ermand Durro <ermand.duro@gmail.com>
2023-10-01 12:43:53 +0200

    Change chatgpt plugin

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -8,4 +8,19 @@ return {
       vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
     end,
   },
+
+  -- CHATGPT
+  -- {
+  --   "jackMort/ChatGPT.nvim",
+  --   config = function()
+  --     require("chatgpt").setup({
+  --       -- optional configuration
+  --     })
+  --   end,
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-telescope/telescope.nvim",
+  --   },
+  -- },
 }
