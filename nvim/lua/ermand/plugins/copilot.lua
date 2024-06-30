33b77b89da3aa548a615472881fc70a7e71e8554
Ermand Durro <ermand.duro@gmail.com>
2024-06-30 22:26:23 +0200

    Delete ermand folder inside nvim config

--- nvim/lua/ermand/plugins/copilot.lua
+++
@@ -1,4 +0,0 @@
-vim.g.copilot_filetypes = { xml = false, markdown = false, yaml = false, plaintext = false }
-vim.g.copilot_no_tap_map = true
-vim.g.copilot_assume_mapped = true
-vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
