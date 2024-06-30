dbfe07514d820e04f773c496958d91ec47d916e2
Ermand Durro <ermand.duro@gmail.com>
2024-06-30 22:27:21 +0200

    Update some nvim plugins

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -4,12 +4,19 @@ return {
   event = "BufReadPre",
   opts = {
     signs = {
-      add = { text = "" },
-      change = { text = "" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "" },
-      untracked = { text = "" },
+      -- add = { text = "" },
+      -- change = { text = "" },
+      -- delete = { text = "" },
+      -- topdelete = { text = "" },
+      -- changedelete = { text = "" },
+      -- untracked = { text = "" },
+
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "+" },
     },
     on_attach = function(buffer)
       local gs = package.loaded.gitsigns
