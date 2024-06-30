75a5273d47b877fb4e9c566e76124bb6b4eaf166
Ermand Durro <ermand.duro@gmail.com>
2024-06-30 22:29:47 +0200

    Update lualine.lua nvim plugin

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,64 +1,83 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  opts = function(plugin)
-    if plugin.override then
-      require("lazyvim.util").deprecate("lualine.override", "lualine.opts")
-    end
-
-    local icons = require("config.icons")
-
-    local navic = {
-      function()
-        return require("nvim-navic").get_location()
-      end,
-      cond = function()
-        return package.loaded["nvim-navic"] and require("nvim-navic").is_available()
-      end,
-    }
-
-    local diagnostics = {
-      "diagnostics",
-      sources = { "nvim_diagnostic" },
-      sections = { "error", "warn", "info", "hint" },
-      symbols = {
-        error = icons.diagnostics.Error,
-        hint = icons.diagnostics.Hint,
-        info = icons.diagnostics.Info,
-        warn = icons.diagnostics.Warn,
-      },
-      colored = true,
-      update_in_insert = false,
-      always_visible = false,
-    }
-
-    local diff = {
-      "diff",
-      symbols = {
-        added = icons.git.added .. " ",
-        untracked = icons.git.added .. " ",
-        modified = icons.git.changed .. " ",
-        removed = icons.git.deleted .. " ",
-      },
-      colored = true,
-      always_visible = false,
-    }
-
-    return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    opts = {
       options = {
+        icons_enabled = true,
         theme = "auto",
-        globalstatus = true,
-        component_separators = { left = "", right = "" },
-        disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
+        component_separators = "|",
+        section_separators = { left = "", right = "" },
       },
-      sections = {
-        lualine_a = { { "mode", separator = { left = "", right = "" } } },
-        lualine_b = {},
-        lualine_c = { diff, diagnostics, navic },
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-      },
-    }
-  end,
+    },
+  },
 }
+
+-- ## Previous config for lualine ##
+-- return {
+--   "nvim-lualine/lualine.nvim",
+--   dependencies = { "nvim-tree/nvim-web-devicons" },
+--   event = "VeryLazy",
+--   opts = function(plugin)
+--     if plugin.override then
+--       require("lazyvim.util").deprecate("lualine.override", "lualine.opts")
+--     end
+--
+--     local icons = require("config.icons")
+--
+--     local navic = {
+--       function()
+--         return require("nvim-navic").get_location()
+--       end,
+--       cond = function()
+--         return package.loaded["nvim-navic"] and require("nvim-navic").is_available()
+--       end,
+--     }
+--
+--     local diagnostics = {
+--       "diagnostics",
+--       sources = { "nvim_diagnostic" },
+--       sections = { "error", "warn", "info", "hint" },
+--       symbols = {
+--         error = icons.diagnostics.Error,
+--         hint = icons.diagnostics.Hint,
+--         info = icons.diagnostics.Info,
+--         warn = icons.diagnostics.Warn,
+--       },
+--       colored = true,
+--       update_in_insert = false,
+--       always_visible = false,
+--     }
+--
+--     local diff = {
+--       "diff",
+--       symbols = {
+--         added = icons.git.added .. " ",
+--         untracked = icons.git.added .. " ",
+--         modified = icons.git.changed .. " ",
+--         removed = icons.git.deleted .. " ",
+--       },
+--       colored = true,
+--       always_visible = false,
+--     }
+--
+--     return {
+--       options = {
+--         theme = "auto",
+--         globalstatus = true,
+--         component_separators = { left = "", right = "" },
+--         disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
+--       },
+--       sections = {
+--         -- lualine_a = { { "mode", separator = { left = "", right = "" } } },
+--         lualine_a = { { "mode", separator = { left = "", right = "" } } },
+--         lualine_b = {},
+--         lualine_c = { diff, diagnostics, navic },
+--         lualine_x = {},
+--         lualine_y = {},
+--         lualine_z = {},
+--       },
+--     }
+--   end,
+-- }
