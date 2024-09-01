1989344456f24212806dd121912a2006d105fa71
Ermand Durro <ermand.duro@gmail.com>
2024-09-01 12:11:23 +0200

    Updating nvim-cmp, nvim-tree and telescope plugins configs

--- nvim/lua/ermand/plugins/nvim-cmp.lua
+++ nvim/lua/ermand/plugins/nvim-cmp.lua
@@ -1,58 +1,89 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		{
-			"L3MON4D3/LuaSnip",
-			-- follow latest release.
-			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-			-- install jsregexp (optional!).
-			build = "make install_jsregexp",
-		},
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"rafamadriz/friendly-snippets", -- useful snippets
-		"onsails/lspkind.nvim",
-	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    {
+      "L3MON4D3/LuaSnip",
+      -- follow latest release.
+      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      -- install jsregexp (optional!).
+      build = "make install_jsregexp",
+    },
+    "saadparwaiz1/cmp_luasnip", -- for autocompletion
+    "rafamadriz/friendly-snippets", -- useful snippets
+    "onsails/lspkind.nvim",
+    {
+      "zbirenbaum/copilot-cmp",
+      config = function()
+        require("copilot_cmp").setup()
+      end,
+    },
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
+    local types = require("luasnip.util.types")
 
-		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-		require("luasnip.loaders.from_vscode").lazy_load()
+    lspkind.init({
+      symbol_map = {
+        Copilot = "",
+      },
+    })
 
-		cmp.setup({
-			completion = {
-				completeopt = "menu,menuone,preview,noselect",
-			},
-			snippet = { -- configure how nvim-cmp interacts with snippet engine
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- snippets
-				{ name = "buffer" }, -- text within current buffer
-				{ name = "path" }, -- file system paths
-			}),
-			mapping = cmp.mapping.preset.insert({
-				["<c-k>"] = cmp.mapping.select_prev_item(),
-				["<c-j>"] = cmp.mapping.select_next_item(),
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<c-space>"] = cmp.mapping.complete(), -- show completion suggestions,
-				["<C-c>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			}),
-			formatting = {
-				format = lspkind.cmp_format({
-					maxwidth = 50,
-					ellipsis_char = "...",
-				}),
-			},
-		})
-	end,
+    -- Display virtual text to indicate snippet has more nodes
+    luasnip.config.setup({
+      ext_opts = {
+        [types.choiceNode] = {
+          active = { virt_text = { { "⇥", "GruvboxRed" } } },
+        },
+        [types.insertNode] = {
+          active = { virt_text = { { "⇥", "GruvboxBlue" } } },
+        },
+      },
+    })
+
+    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    cmp.setup({
+      completion = {
+        completeopt = "menu,menuone,preview,noselect",
+      },
+      snippet = { -- configure how nvim-cmp interacts with snippet engine
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      sources = cmp.config.sources({
+        { name = "copilot", group_index = 2 },
+        { name = "nvim_lsp" },
+        { name = "luasnip" }, -- snippets
+        { name = "buffer" }, -- text within current buffer
+        { name = "path" }, -- file system paths
+      }),
+      mapping = cmp.mapping.preset.insert({
+        ["<c-k>"] = cmp.mapping.select_prev_item(),
+        ["<c-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<c-space>"] = cmp.mapping.complete(), -- show completion suggestions,
+        ["<C-c>"] = cmp.mapping.abort(), -- close completion window
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      }),
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol_text",
+          maxwidth = 70,
+          show_labelDetails = true,
+        }),
+      },
+    })
+  end,
 }
--- nvim/lua/ermand/plugins/telescope.lua
+++ nvim/lua/ermand/plugins/telescope.lua
@@ -1,42 +1,56 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		"nvim-tree/nvim-web-devicons",
-		"folke/todo-comments.nvim",
-	},
-	config = function()
-		local telescope = require("telescope")
-		local actions = require("telescope.actions")
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "nvim-telescope/telescope-frecency.nvim" },
+    "nvim-tree/nvim-web-devicons",
+    "folke/todo-comments.nvim",
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
 
-		telescope.setup({
-			defaults = {
-				-- borderchars = { "", "", "", "", "", "", "", "" },
-				path_display = { "smart" },
-				mappings = {
-					i = {
-						["C-k"] = actions.move_selection_previous,
-						["C-j"] = actions.move_selection_next,
-						["C-q"] = actions.send_selected_to_qflist + actions.open_qflist,
-					},
-				},
-			},
-		})
+    telescope.setup({
+      defaults = {
+        -- borderchars = { "", "", "", "", "", "", "", "" },
+        path_display = { "smart" },
+        mappings = {
+          i = {
+            ["C-k"] = actions.move_selection_previous,
+            ["C-j"] = actions.move_selection_next,
+            ["C-q"] = actions.send_selected_to_qflist + actions.open_qflist,
+          },
+        },
+      },
+      extensions = {
+        frecency = {
+          show_scores = true,
+          show_unindexed = true,
+          show_filter_column = false,
+          ignore_patterns = { "*.git/*", "*/tmp/*" },
+          workspaces = {
+            ["CWD"] = vim.fn.expand("%:p:h"),
+          },
+        },
+      },
+    })
 
-		telescope.load_extension("fzf")
+    telescope.load_extension("fzf")
+    telescope.load_extension("frecency")
 
-		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
-		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
-		vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
-		vim.keymap.set(
-			"n",
-			"<leader>fc",
-			"<cmd>Telescope grep_string<CR>",
-			{ desc = "Find string under the cursor in cwd" }
-		)
+    vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
+    -- vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+    vim.keymap.set("n", "<leader>fr", "<cmd>Telescope frecency workspace=CWD<CR>", { desc = "Fuzzy find recent files" })
+    vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
+    vim.keymap.set(
+      "n",
+      "<leader>fc",
+      "<cmd>Telescope grep_string<CR>",
+      { desc = "Find string under the cursor in cwd" }
+    )
 
-		vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find toods" })
-	end,
+    vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find toods" })
+  end,
 }
